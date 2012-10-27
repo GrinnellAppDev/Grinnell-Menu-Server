ba0083538bc6034960a364ffaa3d46203d07aee8
Colin Tremblay <tremblay@grinnell.edu>
2012-10-27 16:35:34 -0500

    bugfix

--- Nutrition.php
+++ Nutrition.php
@@ -88,8 +88,7 @@ function build_nutrition($dishID, &$json_a){
 			$servSizeStr = $json_a[$dishID]["ServSize"];
 			$output = trim($output, ",")."},";
 
-		$output = $output."{\"ServSize\":\"$servSizeStr\"}";
-		$output = trim($output, ",")."}";
+		$output = $output."\n{\"ServSize\":\"$servSizeStr\"}";
 		return $output;
 	}
 	return null;
