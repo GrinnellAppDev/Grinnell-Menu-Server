bd825f201d3a016224f23d7591c71002b5f4aef2
Colin Tremblay <tremblay@grinnell.edu>
2013-04-05 15:09:18 -0500

    exploration

--- Nutrition.php
+++ Nutrition.php
@@ -67,8 +67,8 @@ return $output;
 }
 
 function build_nutrition($dishID, &$json_a){
-	echo("here");
-	echo($json_a);
+	//echo("here");
+	echo(&$json_a);
 	//$dishID = trim($dishID, ".00");
 	//If the nutrition.json has an entry for the given dish
 	if (isset($json_a[$dishID]))
