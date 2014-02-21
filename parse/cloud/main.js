364ea848b5bfa708af1c61fd580a43b646ceebdd
Colin Tremblay <tremblay@grinnell.edu>
2014-02-21 15:12:15 -0600

    Cloud code gets file from MenuFile object
    gets most MenuFile and pulls the file out of it into a buffer, which can
    be printed to console
    next step: turn csv into a bunch of parse objects

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -6,6 +6,45 @@ Parse.Cloud.define("hello", function(request, response) {
 
 
 Parse.Cloud.job("update_menus", function(request, response) {
-
-	response.success("Menus Updated");
+	var query = new Parse.Query("MenuFile");
+	query.descending("createdAt");
+	query.first({
+		success: function(result) {
+			var file = result.get("file");
+			console.log(file);
+			Parse.Cloud.httpRequest({
+				url: file.url
+			}).then(function(fileResponse) {
+				// The file contents are in response.buffer.
+				console.log(fileResponse.buffer.toString());
+				response.success();
+			});
+		},
+		error: function(error) {
+			response.error("Error getting menu file: " + error.description);
+		}
+	});
 });
+
+// The following code is from stack overflow:
+//  http://stackoverflow.com/questions/8493195/how-can-i-parse-a-csv-string-with-javascript
+// Return array of string values, or NULL if CSV string not well formed.
+function CSVtoArray(text) {
+	var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+	var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+	// Return NULL if input string is not well formed CSV string.
+	if (!re_valid.test(text)) return null;
+	var a = []; // Initialize array to receive values.
+	text.replace(re_value, // "Walk" the string using replace with callback.
+		function(m0, m1, m2, m3) {
+			// Remove backslash from \' in single quoted values.
+			if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+			// Remove backslash from \" in double quoted values.
+			else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+			else if (m3 !== undefined) a.push(m3);
+			return ''; // Return empty string.
+		});
+	// Handle special case of empty last value.
+	if (/,\s*$/.test(text)) a.push('');
+	return a;
+};
