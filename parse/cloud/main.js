a7a52b42ea494e2f555c8158e5f9a9a51423a508
Colin Tremblay <tremblay@grinnell.edu>
2014-02-21 00:01:29 -0600

    Initial work for switching to parse
    file upload works
    haven't tested nutrition file

---
+++ parse/cloud/main.js
@@ -0,0 +1,11 @@
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+	response.success("Hello world!");
+});
+
+
+Parse.Cloud.job("update_menus", function(request, response) {
+
+	response.success("Menus Updated");
+});
