510bafb61c6221520e3703bdc29ae0692c7f2d7c
deweytyl <deweytyl@users.noreply.github.com>
2014-12-03 15:55:39 -0600

    Add header and minor modifications

--- update_daily_dish_lists.php
+++ update_daily_dish_lists.php
@@ -1,10 +1,28 @@
 <?php
 
+/*****************************************************
+ * Tyler Dewey 
+ * Grinnell College '16
+ * 
+ * This file creates .json files that contain
+ * arrays of all dishes available on a given
+ * day in the dining hall
+ * PROCEDURE
+ * 1. Remove old dishes files
+ * 2. Retrieve array of available dates
+ * 3. For each day, create an array to hold that day's dishes
+ * 4. Iterate through each day's meals
+ * 5. Iterate through each meal's stations
+ * 6. Add the dishes from each station to the array
+ * 7. Write that array to a .json file
+ ****************************************************/
+
 echo "Removing old dishes files\n";
 exec("rm *dishes.json");
 
+// Get available dates
 if( ($dates_handle = fopen('dates_array', 'r'))  ==  false ){
-  echo "Failed to open dates file.\n";
+  exit("Failed to open dates file.\n");
   return;
 }
 $dates = unserialize(fread($dates_handle, 50000));
@@ -12,28 +30,22 @@ fclose($dates_handle);
 
 print_r($dates);
 
-/* Create an array of available dishes each day
- * 
- */
+// Create arrays of dishes for each available date
 foreach($dates as $date) {
 	$dishes_path = $date . "_dishes.json";
 	$menu_path = $date . ".json";
 
-	
-
 	if (!file_exists($menu_path)) {
-		echo "failed to find menu file: $menu_path\n";
-		return;
+		exit("failed to find menu file: $menu_path\n");
 	}
 
 	if (!($menu_handle = fopen($menu_path, 'r'))) {
-		echo "failed to open menu file: $menu_path\n";
-		return;
+		exit("failed to open menu file: $menu_path\n");
 	}
 
 	$menu_json = fread($menu_handle, 50000);
 
-	fclose($dates_handle);
+	fclose($menu_handle);
 
 	$menu = json_decode($menu_json);
 	$dishes = array();
@@ -63,8 +75,7 @@ foreach($dates as $date) {
 	}
 
 	if (!($dishes_handle = fopen($dishes_path, 'w'))) {
-		echo "failed to open dishes file: $dishes_path\n";
-		return;
+		exit("failed to open dishes file: $dishes_path\n");
 	}
 	
 	$dishes_json = json_encode($dishes);
